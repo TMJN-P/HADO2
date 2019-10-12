cd2d1aef87472cdde4395fd03f3546ccf3eecb89
TMJN <tomo.hoshii47@gmail.com>
2019-10-13 01:20:37 +0900

    壁貫通バグを直した とてもえらい ではおやすみなさい

--- HADO2/Main.cpp
+++ HADO2/Main.cpp
@@ -10,7 +10,7 @@ public:
 	void draw() {
 		circle.draw(color);
 	}
-	void hado(Vec2 center, int power) {
+	void hado(Vec2& center, int& power) {
 		double a = atan2(circle.y - center.y, circle.x - center.x);
 		Vec2 b;
 		b.x = cos(a) * 1.1 * power / circle.r;
@@ -24,8 +24,8 @@ public:
 	void attenuate() {
 		velocity *= 0.95;
 	}
-	Line line() {
-		return Line({circle.center, beforecenter});
+	bool across(Rect r) {
+		return(circle.center.distanceFrom(beforecenter) > 3 && r.intersects(Line(circle.center, beforecenter)) || r.intersects(circle));
 	}
 };
 
@@ -81,20 +81,21 @@ public:
 
 
 void init(PlayerBall& Red, Ball& Blue, Ball& White, int& RedHado, int& BlueHado) {
-	Red.circle.center = {250, 225};
+	Red.circle.center = Red.beforecenter = {250, 225};
 	Red.velocity = {0, 0};
 	Red.circle.r = 10;
 	Red.color = Palette::Red;
 	
-	Blue.circle.center = {550, 225};
+	Blue.circle.center = Blue.beforecenter = {550, 225};
 	Blue.velocity = {0, 0};
 	Blue.circle.r = 10;
 	Blue.color = Palette::Blue;
 
-	White.circle.center = {400,225};
+	White.circle.center = White.beforecenter = {400, 225};
 	White.velocity = {0, 0};
 	White.circle.r = 4;
 	White.color = Palette::White;
+
 	RedHado = BlueHado = 0;
 }
 
@@ -102,17 +103,30 @@ void Main() {
 	Scene::SetBackground(Palette::Black);
 	Graphics::SetTargetFrameRateHz(60);
 	const Array<Rect> FieldTop {
-		Rect(95, 45, 610, 5), Rect(50, 170, 50, 5), Rect(700, 170, 50, 5)
+		Rect(50, 170, 50, 5), Rect(700, 170, 50, 5), Rect(95, 45, 610, 5)
 	};
 	const Array<Rect> FieldBottom {
-		Rect(95, 400, 610, 5), Rect(50, 275, 50, 5), Rect(700, 275, 50, 5)
+		Rect(50, 275, 50, 5), Rect(700, 275, 50, 5), Rect(95, 400, 610, 5)
 	};
 	const Array<Rect> FieldLeft{
-		Rect(95, 45, 5, 130), Rect(50, 170, 5, 110), Rect(95, 275, 5, 130)
+		Rect(95, 45, 5, 130), Rect(95, 275, 5, 130), Rect(50, 170, 5, 110)
 	};
 	const Array<Rect> FieldRight{
-		Rect(700, 45, 5, 130), Rect(745, 170, 5, 110), Rect(700, 275, 5, 130)
+		Rect(700, 45, 5, 130), Rect(700, 275, 5, 130), Rect(745, 170, 5, 110)
 	};
+	const Array<Rect> FieldTopInvisible{
+		Rect(-200, 170, 300, 5), Rect(700, 170, 300, 5), Rect(-200, 45, 1200, 5)
+	};
+	const Array<Rect> FieldBottomInvisible{
+		Rect(-200, 275, 300, 5), Rect(700, 275, 300, 5), Rect(-200, 400, 1200, 5)
+	};
+	const Array<Rect> FieldLeftInvisible{
+		Rect(95, -200, 5, 375), Rect(95, 275, 5, 380), Rect(50, 0, 5, 450)
+	};
+	const Array<Rect> FieldRightInvisible{
+		Rect(700, -200, 5, 375), Rect(700, 275, 5, 380), Rect(745, 0, 5, 450)
+	};
+
 	const Rect RedGoal(55, 175, 45, 100);
 	Array<Hado> HadoArray;
 	PlayerBall Red;
@@ -124,89 +138,131 @@ void Main() {
 		if (KeySpace.down()) {
 			init(Red, Blue, White, RedHado, BlueHado);
 		}
+		if (MouseR.down()) {
+			White.circle.center = Cursor::Pos();
+		}
 		Circle(Cursor::Pos(), 5).draw(Color(255, 0, 0, 100));
 		Red.reflect = Blue.reflect = White.reflect = false;
 		for (const Rect& r : FieldTop) {
 			r.draw();
-			if (r.intersects(Red.circle) || r.intersects(Red.line())) {
-				Red.velocity.y *= -1.1;
-				Red.circle.y += 1;
-				Red.reflect = true;
-			}
-			if (r.intersects(Blue.circle) || r.intersects(Blue.line())) {
-				Blue.velocity.y *= -1.1;
-				Blue.circle.y += 1;
-				Blue.reflect = true;
-			}
-			if (r.intersects(White.circle) || r.intersects(White.line())) {
-				White.velocity.y *= -1.1;
-				White.circle.y += 1;
-				White.reflect = true;
-			}
-		}
-		for (const Rect& r : FieldBottom) {
+
+		}for (const Rect& r : FieldBottom) {
 			r.draw();
-			if (r.intersects(Red.circle) || r.intersects(Red.line())) {
-				Red.velocity.y *= -1.1;
-				Red.circle.y -= 1;
-				Red.reflect = true;
-			}
-			if (r.intersects(Blue.circle) || r.intersects(Blue.line())) {
-				Blue.velocity.y *= -1.1;
-				Blue.circle.y -= 1;
-				Blue.reflect = true;
-			}
-			if (r.intersects(White.circle) || r.intersects(White.line())) {
-				White.velocity.y *= -1.1;
-				White.circle.y -= 1;
-				White.reflect = true;
-			}
 		}
 		for (const Rect& r : FieldLeft) {
 			r.draw();
-			if (r.intersects(Red.circle) || r.intersects(Red.line())) {
-				Red.velocity.x *= -1.1;
-				Red.circle.x += 1;
-				Red.reflect = true;
-			}
-			if (r.intersects(Blue.circle) || r.intersects(Blue.line())) {
-				Blue.velocity.x *= -1.1;
-				Blue.circle.x += 1;
-				Blue.reflect = true;
-			}
-			if (r.intersects(White.circle) || r.intersects(White.line())) {
-				White.velocity.x *= -1.1;
-				White.circle.x += 1;
-				White.reflect = true;
-			}
 		}
 		for (const Rect& r : FieldRight) {
 			r.draw();
-			if (r.intersects(Red.circle) || r.intersects(Red.line())) {
-				Red.velocity.x *= -1.1;
-				Red.circle.x -= 1;
-				Red.reflect = true;
-			}
-			if (r.intersects(Blue.circle) || r.intersects(Blue.line())) {
-				Blue.velocity.x *= -1.1;
-				Blue.circle.x -= 1;
-				Blue.reflect = true;
-			}
-			if (r.intersects(White.circle) || r.intersects(White.line())) {
-				White.velocity.x *= -1.1;
-				White.circle.x -= 1;
-				White.reflect = true;
-			}
 		}
-		if (Red.reflect) {
+		do {
 			Red.move();
-		}
-		if (Blue.reflect) {
+			Red.reflect = false;
+			for (const Rect& r : FieldTop) {
+				if (Red.across(r)) {
+					Red.velocity.y *= -1.1;
+					Red.circle.y = r.y + r.h + Red.circle.r + 1;
+					Red.reflect = true;
+					break;
+				}
+			}
+			for (const Rect& r : FieldBottom) {
+				if (Red.across(r)) {
+					Red.velocity.y *= -1.1;
+					Red.circle.y = r.y - Red.circle.r - 1;
+					Red.reflect = true;
+					break;
+				}
+			}
+			for (const Rect& r : FieldLeft) {
+				if (Red.across(r)) {
+					Red.velocity.x *= -1.1;
+					Red.circle.x = r.x + r.w + Red.circle.r + 1;
+					Red.reflect = true;
+					break;
+				}
+			}
+			for (const Rect& r : FieldRight) {
+				if (Red.across(r)) {
+					Red.velocity.x *= -1.1;
+					Red.circle.x = r.x - Red.circle.r - 1;
+					Red.reflect = true;
+					break;
+				}
+			}
+		} while (Red.reflect);
+		do {
 			Blue.move();
-		}
-		if (White.reflect) {
+			Blue.reflect = false;
+			for (const Rect& r : FieldTop) {
+				if (Blue.across(r)) {
+					Blue.velocity.y *= -1.1;
+					Blue.circle.y = r.y + r.h + Blue.circle.r + 1;
+					Blue.reflect = true;
+					break;
+				}
+			}
+			for (const Rect& r : FieldBottom) {
+				if (Blue.across(r)) {
+					Blue.velocity.y *= -1.1;
+					Blue.circle.y = r.y - Blue.circle.r - 1;
+					Blue.reflect = true;
+					break;
+				}
+			}
+			for (const Rect& r : FieldLeft) {
+				if (Blue.across(r)) {
+					Blue.velocity.x *= -1.1;
+					Blue.circle.x = r.x + r.w + Blue.circle.r + 1;
+					Blue.reflect = true;
+					break;
+				}
+			}
+			for (const Rect& r : FieldRight) {
+				if (Blue.across(r)) {
+					Blue.velocity.x *= -1.1;
+					Blue.circle.x = r.x - Blue.circle.r - 1;
+					Blue.reflect = true;
+					break;
+				}
+			}
+		} while (Blue.reflect);
+		do {
 			White.move();
-		}
+			White.reflect = false;
+			for (const Rect& r : FieldTop) {
+				if (White.across(r)) {
+					White.velocity.y *= -1.1;
+					White.circle.y = r.y + r.h + White.circle.r + 1;
+					White.reflect = true;
+					break;
+				}
+			}
+			for (const Rect& r : FieldBottom) {
+				if (White.across(r)) {
+					White.velocity.y *= -1.1;
+					White.circle.y = r.y - White.circle.r - 1;
+					White.reflect = true;
+					break;
+				}
+			}
+			for (const Rect& r : FieldLeft) {
+				if (White.across(r)) {
+					White.velocity.x *= -1.1;
+					White.circle.x = r.x + r.w + White.circle.r + 1;
+					White.reflect = true;
+					break;
+				}
+			}
+			for (const Rect& r : FieldRight) {
+				if (White.across(r)) {
+					White.velocity.x *= -1.1;
+					White.circle.x = r.x - White.circle.r - 1;
+					White.reflect = true;
+					break;
+				}
+			}
+		} while (White.reflect);
 		for (Hado& h : HadoArray) {
 			h.draw();
 			h.hit(Red, Blue, White);
@@ -229,12 +285,11 @@ void Main() {
 		Blue.draw();
 		White.draw();
 		Red.cursor();
-		Red.move();
 		Red.attenuate();
-		Blue.move();
 		Blue.attenuate();
-		White.move();
 		White.attenuate();
+
+		Print << White.circle.center << White.velocity;
 		Rect(120, 420, 240, 40).drawFrame(0, 2, Color(255, 200, 200));
 		Rect(120, 420, RedHado, 40).draw(Palette::Red);
 		Rect(440, 420, 240, 40).drawFrame(0, 2, Color(200, 200, 255));
