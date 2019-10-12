5466c1991f11e13991f2c30b846d914be67cb69e
TMJN <tomo.hoshii47@gmail.com>
2019-10-12 14:43:35 +0900

    Hadoの枠を追加 など

--- HADO2/Main.cpp
+++ HADO2/Main.cpp
@@ -4,16 +4,15 @@ class Ball {
 public:
 	Circle circle;
 	Color color;
-	Vec2 center;
 	Vec2 velocity;
 	void Draw() {
-		circle.center = center;
 		circle.draw(color);
 	}
 	void accelerate(Vec2 a) {
+		velocity += a;
 	}
 	void move() {
-		center += velocity;
+		circle.center += velocity;
 	}
 	void attenuate() {
 		velocity *= 0.95;
@@ -23,13 +22,13 @@ public:
 class PlayerBall : public Ball {
 public:
 	void accelerate() {
-		if (center.distanceFrom(Cursor::Pos()) < 10) {
+		if (circle.center.distanceFrom(Cursor::Pos()) < 10) {
 			velocity *= 0.9;
 		}
 		else {
 			Vec2 b;
-			b.x = cos(atan2(Cursor::Pos().y - center.y, Cursor::Pos().x - center.x)) * 0.4;
-			b.y = sin(atan2(Cursor::Pos().y - center.y, Cursor::Pos().x - center.x)) * 0.4;
+			b.x = cos(atan2(Cursor::Pos().y - circle.center.y, Cursor::Pos().x - circle.center.x)) * 0.4;
+			b.y = sin(atan2(Cursor::Pos().y - circle.center.y, Cursor::Pos().x - circle.center.x)) * 0.4;
 			velocity += b;
 		}
 	}
@@ -38,14 +37,26 @@ public:
 class Hado {
 public:
 	Vec2 center;
-	double radius;
+	int radius, power;
+	bool hitred, hitblue, hitwhite;
+	Color color;
+	void draw() {
+
+	}
+	void hit() {
+
+	}
 };
 
-void init(PlayerBall& Red, Ball& Blue, Ball& White, double& RedHado, double& BlueHado) {
-	Red.center = {300, 200};
+
+void init(PlayerBall& Red, Ball& Blue, Ball& White, int& RedHado, int& BlueHado) {
+	Red.circle.center = {300, 200};
 	Red.velocity = {0, 0};
 	Red.circle.r = 10;
 	Red.color = Palette::Red;
+
+
+	RedHado = BlueHado = 0;
 }
 
 void Main() {
@@ -64,46 +75,60 @@ void Main() {
 	const Array<Line> FieldRight {
 		Line(700, 50, 700, 175), Line(740, 175, 740, 275), Line(700, 275, 700, 400)
 	};
-	Array<Hado>{
-
-	};
+	Array<Hado> HadoArray;
 	PlayerBall Red;
 	Ball Blue, White;
-	double RedHado, BlueHado;
+	int RedHado, BlueHado;
 	init(Red, Blue, White, RedHado, BlueHado);
 	while (System::Update()) {
+		Red.Draw();
+		Circle(Cursor::Pos(), 5).draw(Color(255, 0, 0, 100));
 		for (const Line& l : FieldTop) {
 			l.draw(3);
 			if (l.intersects(Red.circle)) {
 				Red.velocity.y = -Red.velocity.y;
-				Red.center.y += 1;
+				Red.circle.center.y += 1;
 			}
 		}
 		for (const Line& l : FieldBottom) {
 			l.draw(3);
 			if (l.intersects(Red.circle)) {
 				Red.velocity.y = -Red.velocity.y;
-				Red.center.y -= 1;
+				Red.circle.center.y -= 1;
 			}
 		}
 		for (const Line& l : FieldLeft) {
 			l.draw(3);
 			if (l.intersects(Red.circle)) {
 				Red.velocity.x = -Red.velocity.x;
-				Red.center.x += 1;
+				Red.circle.center.x += 1;
 			}
 		}
 		for (const Line& l : FieldRight) {
 			l.draw(3);
 			if (l.intersects(Red.circle)) {
 				Red.velocity.x = -Red.velocity.x;
-				Red.center.x -= 1;
+				Red.circle.center.x -= 1;
 			}
 		}
+		for (Hado& h : HadoArray) {
+			h.draw();
+			h.hit();
+		}
+
+		if (MouseL.pressed()) {
+			RedHado++;
+		}
+		if (MouseL.up()) {
+			RedHado = 0;
+			//ゆっくり減るよう あとで調整
+		}
+		RedHado = Min(RedHado, 250);
+
 		Red.accelerate();
 		Red.move();
-		Red.Draw();
 		Red.attenuate();
-		Circle(Cursor::Pos(), 5).draw(Color(255, 0, 0, 100));
+		Rect(100, 420, 250, 40).drawFrame(0, 5);
+		Rect(100, 420, RedHado, 40).draw(Palette::Red);
 	}
 }
